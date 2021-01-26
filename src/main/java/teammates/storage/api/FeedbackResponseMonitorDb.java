6bf40541befb352170f4b1a0e0d33de24b545221
daongochieu2810 <daongochieu2810@yahoo.com.vn>
2021-01-26 17:58:46 +0800

    Change as requested

--- src/main/java/teammates/storage/api/FeedbackResponseMonitorDb.java
+++ src/main/java/teammates/storage/api/FeedbackResponseMonitorDb.java
@@ -45,8 +45,7 @@ public class FeedbackResponseMonitorDb extends EntitiesDb<FeedbackResponseRecord
      * Gets a set of response records with chosen duration and interval.
      */
     public List<FeedbackResponseRecordAttributes> getResponseRecords(long duration, long interval) {
-        long currentTimeInMilisec = System.currentTimeMillis();
-        long startTimeInMilisec = currentTimeInMilisec - duration;
+        long startTimeInMilisec = System.currentTimeMillis() - duration;
 
         List<Key<FeedbackResponseRecord>> keysOfRecords = load().keys().list();
         List<FeedbackResponseRecord> records = new ArrayList<>();
